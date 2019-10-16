5b0c2a75efe000835ba834847e405bb15dc02d48
Jonatas Alves da Silva <jonatadev@gmail.com>
2019-10-15 21:37:50 -0300

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+package hello;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
+	
+    public String Hello() {
+    	return "Hello World";
+    }
+    
+    public void HelloMethod() {
+    	System.out.println(" Seja Bem Vindo Jonatas Alves da Silva!");
+    }
+}
---
+++ Jonatas.java
@@ -0,0 +1,5 @@
+package hello;
+
+public class Jonatas {
+	private String name;
+}
